9331e5e61a4aca47a400b9fd6a27427835f68dec
roynatech2544 <whiteshell2544@naver.com>
2023-03-16 08:11:13 +0000

    kernel: add simple script
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

---
+++ build_kernel.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+set -e
+
+[ ! -e "scripts/packaging/pack.sh" ] && git submodule init && git submodule update
+[ ! -e "toolchain" ] && echo "Make toolchain avaliable at $(pwd)/toolchain" && exit
+
+# Patch for 4.14
+sed -i 's/#ifdef CONFIG_KPROBES/#if 0/g' KernelSU/kernel/ksu.c
+
+export KBUILD_BUILD_USER=Ash
+export KBUILD_BUILD_HOST=GrassLand
+
+PATH=$PWD/toolchain/bin:$PATH
+
+rm -rf out
+make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) vendor/chime_defconfig
+make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc)
