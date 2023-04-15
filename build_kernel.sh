271b76bab617b8d1bfde5be50dc5d6c53b252b2b
hac4us06 <hac4usanime@gmail.com>
2023-04-15 23:19:11 +0000

    ARM64: DTS: add lime support back
    This reverts commit 66bcc5254d355b8d13bcc84a1d1e759540ebd665.
    
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -14,4 +14,8 @@ PATH=$PWD/toolchain/bin:$PATH
 
 rm -rf out
 make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) vendor/chime_defconfig
-make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc)
+make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 PRODUCT_DEVICE=lime -j$(nproc) dtbo.img
+mv out/arch/arm64/boot/dtbo.img out/dtbo_lime.img
+make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 PRODUCT_DEVICE=citrus -j$(nproc) dtbo.img
+mv out/arch/arm64/boot/dtbo.img out/dtbo_citrus.img
+make O=out CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 PRODUCT_DEVICE=lime -j$(nproc)
--- scripts/post-build-hook.sh
+++ scripts/post-build-hook.sh
@@ -50,3 +50,6 @@ cd ${srctree}/scripts/packaging/
 bash pack.sh "${MY_PWD}/${OBJ}" "${KERNELZIP}"
 tg_sendText "<b>${KERNELSTR} Kernel Build</b>%0ABuild ended <code>Target: ${OBJ}</code>%0AFor device ${DEVICE}%0Abranch <code>${BRANCH}</code>%0AUnder commit <code>${COMMITMSG}</code>%0AUsing compiler: <code>${CCSTR}</code>%0AEnded on <code>$(date)</code>"
 tg_sendFile "${KERNELZIP}"
+for i in $MY_PWD/dtbo_*; do
+	tg_sendFile $i
+done
